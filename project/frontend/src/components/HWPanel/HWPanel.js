dc1607b28731add76b7f839b90721d62e0bc4f36
shaunakdivine <shaunak.divine@gmail.com>
2022-10-27 09:44:02 -0500

    added bootstrap input bar

--- project/frontend/src/components/HWPanel/HWPanel.js
+++ project/frontend/src/components/HWPanel/HWPanel.js
@@ -1,5 +1,6 @@
 
 import Button from 'react-bootstrap/Button';
+import { InputGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './HWPanel.css';
 import React from 'react';
@@ -23,7 +24,12 @@ class HWPanel extends React.Component {
         
                 </div>
                 <div className = "HWPanel-buttons">
-                    <p> Enter Quantity </p>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="inputGroup-sizing-default">Enter Quantity:</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"></input>
+                    </div>
                     <Button> Check In</Button>
                     <Button> Check Out</Button>
 
