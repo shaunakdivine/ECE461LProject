3c9097abe97d95ee18cd6359409f4312916a0a17
stephensyoung <70411831+stephensyoung@users.noreply.github.com>
2022-10-26 19:59:30 -0500

    Basic Search Bar

--- project/frontend/src/pages/Projects.jsx
+++ project/frontend/src/pages/Projects.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { BasePage } from '../components/utility';
+import { Button, Containers,  ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
 
 function ProjectsPage(props) {
   return (
     <BasePage title='Projects'>
-      <div>put all the components here</div>
+      <div class = "container-fluid"> 
+        <input type = "search" class = "form-control rounder"  placeholder = "Search" aria-label = "Search" aria-describedby = "search-addon"/>
+        <button type = "button" class = "btn btn-outline-primary">search</button>
+      </div>
     </BasePage>
   )
 }
