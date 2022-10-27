51b4119cc4167d43e3c8753ed28f821104ecf705
stephensyoung <70411831+stephensyoung@users.noreply.github.com>
2022-10-26 20:11:41 -0500

    Search Bar + Shaunak Code

--- project/frontend/src/pages/Projects.jsx
+++ project/frontend/src/pages/Projects.jsx
@@ -8,10 +8,10 @@ import ProjectPanel from '../components/Projects/ProjectsComponent';
 function ProjectsPage(props) {
   return (
     <BasePage title='Projects'>
-      {/* <div class = "container-fluid"> 
+      <div class = "container-fluid"> 
         <input type = "search" class = "form-control rounder"  placeholder = "Search" aria-label = "Search" aria-describedby = "search-addon"/>
         <button type = "button" class = "btn btn-outline-primary">search</button>
-      </div> */}
+      </div> 
       <div className="project-container">
         <div className="project-panel">
           <ProjectPanel projectName="Project 1"> Project 1</ProjectPanel>
