4a91e9d82b98b84ccd319e9961cc2f162dba84d5
shaunakdivine <shaunak.divine@gmail.com>
2022-10-26 19:57:52 -0500

    added very rough draft of projects page

--- project/frontend/src/pages/Projects.jsx
+++ project/frontend/src/pages/Projects.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { BasePage } from '../components/utility';
+import { Container } from 'react-bootstrap';
+import ProjectPanel from '../components/Projects/ProjectsComponent';
+
 
 function ProjectsPage(props) {
   return (
     <BasePage title='Projects'>
-      <div>put all the components here</div>
+      <div className="project-container">
+        <h1 className = "project-header"> Projects </h1>
+        <div className="project-panel">
+          <ProjectPanel projectName="Project 1"> Project 1</ProjectPanel>
+          <ProjectPanel projectName="Project 2"> Project 2</ProjectPanel> 
+          <ProjectPanel projectName="Project 3"> Project 3</ProjectPanel>
+    
+        </div>
+      </div>  
     </BasePage>
   )
 }
