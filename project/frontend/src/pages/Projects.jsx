4b714a5002c0803cf9b25fab8f0fe79f8e4160e4
shaunakdivine <shaunak.divine@gmail.com>
2022-10-26 20:08:02 -0500

    kept stephen work commented out

--- project/frontend/src/pages/Projects.jsx
+++ project/frontend/src/pages/Projects.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import { BasePage } from '../components/utility';
-<<<<<<< HEAD
 import { Button, Containers,  ButtonGroup, DropdownButton, MenuItem } from 'react-bootstrap';
-=======
 import { Container } from 'react-bootstrap';
 import ProjectPanel from '../components/Projects/ProjectsComponent';
 
->>>>>>> 44617d07d0d7ce64a6e68c40432eacc014189c69
 
 function ProjectsPage(props) {
   return (
     <BasePage title='Projects'>
-<<<<<<< HEAD
-      <div class = "container-fluid"> 
+      {/* <div class = "container-fluid"> 
         <input type = "search" class = "form-control rounder"  placeholder = "Search" aria-label = "Search" aria-describedby = "search-addon"/>
         <button type = "button" class = "btn btn-outline-primary">search</button>
-      </div>
-=======
+      </div> */}
       <div className="project-container">
         <h1 className = "project-header"> Projects </h1>
         <div className="project-panel">
@@ -26,7 +21,6 @@ function ProjectsPage(props) {
     
         </div>
       </div>  
->>>>>>> 44617d07d0d7ce64a6e68c40432eacc014189c69
     </BasePage>
   )
 }
