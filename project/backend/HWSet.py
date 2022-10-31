60dbdad0b7acc0a930f3c1374f04a03d6ae59f4d
carloskikotrevino <carlos.trevino359@utexas.edu>
2022-10-31 00:54:38 -0500

    Updated HWSet.py and Projects.py
    Each method updates corresponding MongoDB collection and specific document, needs to be revised and looked over to make sure it matches API specification

--- project/backend/HWSet.py
+++ project/backend/HWSet.py
@@ -8,9 +8,21 @@ collectionOne = database["HWSet"]
 
 class HWSet:
 
-    def __init__(self, qty):
+    def __init__(self, name, qty):
+        self.__name = name
         self.__capacity = qty
         self.__availability = self.capacity
+        new_HWSet = {
+            "Name": self.__name,
+            "Capacity": self.__capacity,
+            "Availability": self.__availability
+        }
+        collectionOne.insert_one(new_HWSet)
+
+    def __del__(self):
+        myquery = {"Name": self.__name}
+        collectionOne.delete_one(myquery)
+        pass
 
     # accessor function to return the number of unused units
     def get_availability(self):
@@ -30,14 +42,28 @@ class HWSet:
     # in the following manner: Allow users to check out the number of units that are
     # available and then return error = -1.
     def check_out(self, qty):
+        myquery = {"Name": self.__name}
+        #mydoc = collectionOne.find_one(myquery)
+    
         if qty > self.__availability:
             self.__availability = 0
+            new_availability = {"$set": {"Availability": self.__availability}}
+            collectionOne.update_one(myquery, new_availability)
             return -1
         else:
             self.__availability = self.__availability - qty
+            new_availability = {"$set": {"Availability": self.__availability}}
+            collectionOne.update_one(myquery, new_availability)
             return 0
 
     # method that checks in number of units specified by qty. This method should update
     # the number of units available after check_in. No error checking is required here.
     def check_in(self, qty):
         self.__availability = self.__availability + qty
+        myquery = {"Name": self.__name}
+        new_availability = {"$set": {"Availability": self.__availability}}
+        collectionOne.update_one(myquery, new_availability)
+
+def list_HWSets():
+    for x in collectionOne.find({}, {"Name":1, "Capacity": 1, "Availability":1}):
+        print(x)
--- project/backend/Projects.py
+++ project/backend/Projects.py
@@ -12,6 +12,16 @@ class Projects:
         self.__name = name
         self.__ID = ID
         self.__description = description
+        new_project = {
+            "Name": self.__name,
+            "ID": self.__ID,
+            "Description": self.__description
+        }
+        collectionTwo.insert_one(new_project)
+
+    def __del__(self):
+        myquery = {"Name": self.__name}
+        collectionTwo.delete_one(myquery)
 
     def get_name(self):
         return self.__name
@@ -23,10 +33,30 @@ class Projects:
         return self.__description
 
     def change_name(self, new_name):
+        myquery = {"Name": self.__name}
+        #mydoc = collectionTwo.find_one(myquery)
+        newvalues = {"$set": {"Name": new_name}}
+        collectionTwo.update_one(myquery, newvalues)
+
         self.__name = new_name
 
+
     def change_ID(self, new_ID):
+        myquery = {"ID": self.__ID}
+        #mydoc = collectionTwo.find_one(myquery)
+        newvalues = {"$set": {"ID": new_ID}}
+        collectionTwo.update_one(myquery, newvalues)
+
         self.__ID = new_ID
 
     def change_description(self, new_description):
+        myquery = {"Description": self.__description}
+        #mydoc = collectionTwo.find_one(myquery)
+        newvalues = {"$set": {"Description": new_description}}
+        collectionTwo.update_one(myquery, newvalues)
+
         self.__description = new_description
+
+def get_projects():
+    for x in collectionTwo.find({}, {"Name":1, "ID": 1, "Description":1}):
+        print(x)
