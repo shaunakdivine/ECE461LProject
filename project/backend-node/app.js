405f3427b955ff3e1e67e47fa5f369e44972973f
Han-Hsuan Lin <hhl@utexas.edu>
2022-10-31 01:02:20 -0500

    release: v0.0.1

--- project/backend-node/app.js
+++ project/backend-node/app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
 const morgan = require('morgan');
 const accountRouter = require('./routes/account');
 
 const app = express();
+app.use(express.static(path.join(__dirname, "build")));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
 // routers
 app.use('/api/account', accountRouter);
+
+// default pages
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./build/index.html"));
+});
 
 //This is the port number below (David Gross)
 app.listen(5001, () => {
